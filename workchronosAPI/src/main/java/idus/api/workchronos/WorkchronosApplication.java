6c52634f6d58ffb05d7f9330f8c6766e048995cc
aldcejam <aldcejam.pro@gmail.com>
2024-12-16 19:17:46 -0300

    first commit

---
+++ workchronosAPI/src/main/java/idus/api/workchronos/WorkchronosApplication.java
@@ -0,0 +1,13 @@
+package idus.api.workchronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkchronosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkchronosApplication.class, args);
+	}
+
+}
---
+++ workchronosAPI/src/test/java/idus/api/workchronos/WorkchronosApplicationTests.java
@@ -0,0 +1,13 @@
+package idus.api.workchronos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkchronosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
