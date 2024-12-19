8dde8968b7913fa9ee42eac596ce277ddd65ee6e
aldcejam <aldcejam.pro@gmail.com>
2024-12-19 20:54:18 -0300

    feat: update WorkBreak class to use Date for start and end times, and add description field

--- workchronosAPI/src/main/java/idus/api/workchronos/domain/workManagment/WorkBreak.java
+++ workchronosAPI/src/main/java/idus/api/workchronos/domain/workManagment/WorkBreak.java
@@ -5,14 +5,25 @@ import lombok.Getter;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
+import java.util.Date;
 
 @Getter
 public class WorkBreak extends ValueObject {
-    private final ZonedDateTime start;
-    private final ZonedDateTime end;
+    private final Date start;
+    private final Date end;
+    private final String description;
 
-    public WorkBreak(ZonedDateTime start, ZonedDateTime end) {
+    public WorkBreak(Date start, Date end, String description) {
         this.start = start;
         this.end = end;
+        this.description = description;
+    }
+
+    public static WorkBreak create(Date start, Date end) {
+        return new WorkBreak(start, end, null);
+    }
+
+    public static WorkBreak create(Date start, Date end, String description) {
+        return new WorkBreak(start, end, description);
     }
 }
