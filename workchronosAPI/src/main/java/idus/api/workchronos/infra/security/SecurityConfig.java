0c5d17fcdec8adb78229d9c97e4a5f24504f0e17
aldcejam <aldcejam.pro@gmail.com>
2024-12-21 07:09:33 -0300

    feat: add ApiTestController for testing API functionality

---
+++ workchronosAPI/src/main/java/idus/api/workchronos/infra/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package idus.api.workchronos.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/user").permitAll()
+                        .anyRequest().permitAll())
+                .build();
+    }
+}
