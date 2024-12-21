f3fdb072fc03378cf242f479b12e1961595465d7
aldcejam <aldcejam.pro@gmail.com>
2024-12-21 12:23:47 -0300

    feat: add query to find latest attendance record by user

--- workchronosAPI/src/main/java/idus/api/workchronos/infra/persistence/attendanceRecord/AttendanceRecordRepository.java
+++ workchronosAPI/src/main/java/idus/api/workchronos/infra/persistence/attendanceRecord/AttendanceRecordRepository.java
@@ -1,8 +1,13 @@
 package idus.api.workchronos.infra.persistence.attendanceRecord;
 
+import idus.api.workchronos.infra.persistence.user.UserDB;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AttendanceRecordRepository extends JpaRepository<AttendanceRecordDB, UUID> {
+    @Query("SELECT ar FROM AttendanceRecordDB ar WHERE ar.user = :user ORDER BY ar.createdAt DESC")
+    Optional<AttendanceRecordDB> findLatestByUser(UserDB user);
 }
