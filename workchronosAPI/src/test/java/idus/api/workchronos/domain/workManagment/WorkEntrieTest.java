b16f8deae500bdddad9cf9ab7ab8c7683b4ba65c
aldcejam <aldcejam.pro@gmail.com>
2024-12-21 12:21:43 -0300

    refactor: update WorkEntrieTest to use constructor instead of factory method for instance creation

--- workchronosAPI/src/test/java/idus/api/workchronos/domain/workManagment/WorkEntrieTest.java
+++ workchronosAPI/src/test/java/idus/api/workchronos/domain/workManagment/WorkEntrieTest.java
@@ -16,7 +16,7 @@ public class WorkEntrieTest {
         final var expectedWorkEnd = LocalTime.of(17, 0);
         final var expectedBreaks = List.of(WorkBreak.create(LocalTime.of(12, 0), LocalTime.of(12, 30)));
 
-        final var actualWorkEntrie = WorkEntrie.create(expectedWorkStart, expectedWorkEnd, expectedBreaks);
+        final var actualWorkEntrie = new WorkEntrie(expectedWorkStart, expectedWorkEnd, expectedBreaks);
 
         Assertions.assertNotNull(actualWorkEntrie);
         Assertions.assertEquals(expectedWorkStart, actualWorkEntrie.getWorkStart());
@@ -33,7 +33,7 @@ public class WorkEntrieTest {
         Assertions.assertNotNull(actualWorkEntrie);
         Assertions.assertEquals(expectedWorkStart, actualWorkEntrie.getWorkStart());
         Assertions.assertNull(actualWorkEntrie.getWorkEnd());
-        Assertions.assertNull(actualWorkEntrie.getBreaks());
+        Assertions.assertTrue(actualWorkEntrie.getBreaks().isEmpty());
     }
 
     @Test
@@ -47,7 +47,7 @@ public class WorkEntrieTest {
 
     @Test
     public void givenWorkAlreadyEnded_whenEndWork_thenThrowException() {
-        final var workEntrie = WorkEntrie.create(
+        final var workEntrie = new WorkEntrie(
                 LocalTime.of(9, 0),
                 LocalTime.of(17, 0),
                 new ArrayList<>()
@@ -71,7 +71,7 @@ public class WorkEntrieTest {
     public void givenBreakInProgress_whenStartNewBreak_thenThrowException() {
         final var breaks = new ArrayList<WorkBreak>();
         breaks.add(WorkBreak.create(LocalTime.of(10, 0), null));
-        final var workEntrie = WorkEntrie.create(LocalTime.of(9, 0), null, breaks);
+        final var workEntrie = new WorkEntrie(LocalTime.of(9, 0), null, breaks);
 
         final var exception = Assertions.assertThrows(IllegalStateException.class, () -> workEntrie.startBreak(LocalTime.of(11, 0)));
 
@@ -80,7 +80,7 @@ public class WorkEntrieTest {
 
     @Test
     public void givenNoBreaks_whenEndBreak_thenThrowException() {
-        final var workEntrie = WorkEntrie.create(LocalTime.of(9, 0), null, new ArrayList<>());
+        final var workEntrie = new WorkEntrie(LocalTime.of(9, 0), null, new ArrayList<>());
 
         final var exception = Assertions.assertThrows(IllegalStateException.class, () -> workEntrie.endBreak(LocalTime.of(10, 0)));
 
@@ -93,7 +93,7 @@ public class WorkEntrieTest {
         final var breakEnd = LocalTime.of(10, 30);
         final var breaks = new ArrayList<WorkBreak>();
         breaks.add(WorkBreak.create(breakStart, null));
-        final var workEntrie = WorkEntrie.create(LocalTime.of(9, 0), null, breaks);
+        final var workEntrie = new WorkEntrie(LocalTime.of(9, 0), null, breaks);
 
         workEntrie.endBreak(breakEnd);
 
@@ -110,7 +110,7 @@ public class WorkEntrieTest {
                 }
                 """;
 
-        final var actualWorkEntrie = WorkEntrie.fromJson(json);
+        final var actualWorkEntrie = WorkEntrie.fromString(json);
 
         Assertions.assertNotNull(actualWorkEntrie);
         Assertions.assertEquals(LocalTime.of(9, 0), actualWorkEntrie.getWorkStart());
