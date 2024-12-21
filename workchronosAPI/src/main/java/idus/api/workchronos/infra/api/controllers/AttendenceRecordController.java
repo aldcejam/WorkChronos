7defa8d4c24fe2c366248cc7907226c13891ef64
aldcejam <aldcejam.pro@gmail.com>
2024-12-21 12:25:01 -0300

    feat: add AttendenceRecordController to handle attendance record operations

---
+++ workchronosAPI/src/main/java/idus/api/workchronos/infra/api/controllers/AttendenceRecordController.java
@@ -0,0 +1,25 @@
+package idus.api.workchronos.infra.api.controllers;
+
+import idus.api.workchronos.application.AttendenceRecordService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController()
+@RequestMapping("/attendance-record")
+public class AttendenceRecordController {
+
+    AttendenceRecordService attendenceRecordService;
+
+    public AttendenceRecordController(AttendenceRecordService attendenceRecordService) {
+        this.attendenceRecordService = attendenceRecordService;
+    }
+
+    @PostMapping("/{userId}/start-day")
+    public void startDay(@PathVariable UUID userId) {
+        this.attendenceRecordService.startDay(userId);
+    }
+}
