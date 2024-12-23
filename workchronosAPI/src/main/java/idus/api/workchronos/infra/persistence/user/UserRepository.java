40e83e1c45bf9606f5bd161ebbf7c2b2de3bcc89
aldcejam <aldcejam.pro@gmail.com>
2024-12-23 12:15:44 -0300

    feat: remove deprecated Docker Compose configuration and add CORS configuration for API

--- workchronosAPI/src/main/java/idus/api/workchronos/infra/persistence/user/UserRepository.java
+++ workchronosAPI/src/main/java/idus/api/workchronos/infra/persistence/user/UserRepository.java
@@ -2,9 +2,11 @@ package idus.api.workchronos.infra.persistence.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserDB, UUID> {
     UserDetails findByEmail(String email);
     boolean existsByEmail(String email);
